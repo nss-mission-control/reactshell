2a3fa59e5ce4e958a3f3d1670a5c16504a8f880a
kellymorin <kmorin06@gmail.com>
2018-11-28 10:58:33 -0600

    Updated with API manager from previous nutshell project.

---
+++ src/modules/APIManager.js
@@ -0,0 +1,44 @@
+const URL = "http://localhost:8088/"
+
+export default class APIManager{
+  getAllCategory(category) {
+    return fetch(`${URL}${category}`)
+      .then(entries => entries.json())
+  }
+
+  getOneFromCategory(category, id) {
+    return fetch(`${URL}${category}/${id}`)
+      .then(inputs => inputs.json())
+  }
+
+  saveItem(category, item) {
+    return fetch(`${URL}${category}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(item)
+    }
+    ).then(jsonData => jsonData.json())
+  }
+
+  deleteItem(category, id) {
+    return fetch(`${URL}${category}/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  }
+
+  updateItem(category, id, item){
+    return fetch(`${URL}${category}/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(item)
+    }
+    )
+  }
+}
