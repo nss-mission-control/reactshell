589109c4a942ea83e053e83307b172edc06d40dc
Brendan McCray <rbmccr@gmail.com>
2018-11-30 09:58:13 -0600

    finished adding routes to applicationviews. Now need to pass props once merges take place

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -3,7 +3,10 @@ import React, { Component } from "react"
 import Messages from "./messages"
 import LogIn from "./login/LogIn"
 import SignUp from "./login/SignUp"
-
+import Tasks from "./tasks/Tasks"
+import Following from "./friends/Friends"
+import Events from "./events/Events"
+import News from "./news/News"
 
 export default class ApplicationViews extends Component {
 
@@ -16,6 +19,34 @@ export default class ApplicationViews extends Component {
         <Route exact path="/" render={(props) => {
           if (this.isAuthenticated()) {
             return <Messages messages={this.props.messages} refresh={this.props.refresh} />
+          } else {
+            return <LogIn {...props} activeUser={this.props.activeUser} />
+          }
+        }} />
+        <Route exact path="/tasks" render={(props) => {
+          if (this.isAuthenticated()) {
+            return <Tasks />
+          } else {
+            return <LogIn {...props} activeUser={this.props.activeUser} />
+          }
+        }} />
+        <Route exact path="/events" render={(props) => {
+          if (this.isAuthenticated()) {
+            return <Events />
+          } else {
+            return <LogIn {...props} activeUser={this.props.activeUser} />
+          }
+        }} />
+        <Route exact path="/news" render={(props) => {
+          if (this.isAuthenticated()) {
+            return <News />
+          } else {
+            return <LogIn {...props} activeUser={this.props.activeUser} />
+          }
+        }} />
+        <Route exact path="/following" render={(props) => {
+          if (this.isAuthenticated()) {
+            return <Following />
           } else {
             return <LogIn {...props} activeUser={this.props.activeUser} />
           }
@@ -34,30 +65,3 @@ export default class ApplicationViews extends Component {
     )
   }
 }
-
-  // Check if credentials are in local storage
-//   isAuthenticated = () => sessionStorage.getItem("id") !== null
-
-//   render() {
-//     if (this.props.loggedIn) {
-//       return (
-//         <React.Fragment>
-//           <Route exact path="/" render={(props) => {
-//             return <Messages messages={this.props.messages} refresh={this.props.refresh} />
-//           }} />
-//         </React.Fragment>
-//       )
-//     } else {
-//       return (
-//         <React.Fragment>
-//           <Route exact path="/signup" render={(props) => {
-//             return <SignUp activeUser={this.props.activeUser} refreshData={this.props.refreshData} login={this.props.login} />
-//           }} />
-//           <Route exact path="/" render={(props) => {
-//             return <LogIn activeUser={this.props.activeUser} />
-//           }} />
-//         </React.Fragment>
-//       )
-//     }
-//   }
-// }
